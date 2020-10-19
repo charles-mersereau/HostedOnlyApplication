cc4528256e3c5f41aea3fb86488ac45bf30b82af
charles-mersereau <cmersereau@waysuninc.com>
2020-10-19 14:18:59 -0400

    initial commit

---
+++ app/src/main/java/com/waysuninc/hostedonlyapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.waysuninc.hostedonlyapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
